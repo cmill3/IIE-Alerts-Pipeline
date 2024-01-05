c7873cecda0073f6a4a07db956149e256ff6ee2b
cm3 <cfmiller1397@gmail.com>
2024-01-05 11:13:22 -0500

    local

--- historical_dataset_builder.py
+++ historical_dataset_builder.py
@@ -79,7 +79,7 @@ def build_historic_data(date_str):
         # new_df = pd.concat([old_df,df],ignore_index=True)
         # new_df = new_df.drop_duplicates(subset=['symbol'])
         # new_df.drop(columns=['Unnamed: 0'], inplace=True)
-        put_response = s3.put_object(Bucket="inv-alerts", Key=f"full_alerts/{key_str}/{hour}.csv", Body=new_df.to_csv())
+        put_response = s3.put_object(Bucket="inv-alerts", Key=f"full_alerts/{key_str}/{hour}.csv", Body=df.to_csv())
     return put_response
     
 def generate_dates_historic(date_str):
@@ -96,20 +96,24 @@ def combine_hour_aggs(aggregates, hour_aggregates, hour):
     full_aggs = []
     hour_df = pd.concat(hour_aggregates)
     for index, value in enumerate(aggregates):
-        hour_aggs = hour_df.loc[hour_df["symbol"] == value.iloc[0]['symbol']]
-        hour_aggs = hour_aggs.loc[hour_aggs["hour"] < int(hour)]
-        if len(hour_aggs) > 1:
-            hour_aggs = hour_aggs.iloc[:-1]
-        volume = hour_aggs.v.sum()
-        open = hour_aggs.o.iloc[0]
-        close = hour_aggs.c.iloc[-1]
-        high = hour_aggs.h.max()
-        low = hour_aggs.l.min()
-        n = hour_aggs.n.sum()
-        t = hour_aggs.t.iloc[-1]
-        aggs_list = [volume, open, close, high, low, hour_aggs.date.iloc[-1], hour,hour_aggs.symbol.iloc[-1],t]
-        value.loc[len(value)] = aggs_list
-        full_aggs.append(value)
+        try:
+            hour_aggs = hour_df.loc[hour_df["symbol"] == value.iloc[0]['symbol']]
+            hour_aggs = hour_aggs.loc[hour_aggs["hour"] < int(hour)]
+            if len(hour_aggs) > 1:
+                hour_aggs = hour_aggs.iloc[:-1]
+            volume = hour_aggs.v.sum()
+            open = hour_aggs.o.iloc[0]
+            close = hour_aggs.c.iloc[-1]
+            high = hour_aggs.h.max()
+            low = hour_aggs.l.min()
+            n = hour_aggs.n.sum()
+            t = hour_aggs.t.iloc[-1]
+            aggs_list = [volume, open, close, high, low, hour_aggs.date.iloc[-1], hour,hour_aggs.symbol.iloc[-1],t]
+            value.loc[len(value)] = aggs_list
+            full_aggs.append(value)
+        except Exception as e:
+            print(f"combine hour aggs {e}")
+            print(value)
     return full_aggs
 
 
@@ -129,7 +133,7 @@ def pull_df(date_stamp, prefix, hour):
 if __name__ == "__main__":
     # build_historic_data(None, None)
     cpu = os.cpu_count()
-    start_date = datetime(2021,1,1)
+    start_date = datetime(2022,3,5)
     end_date = datetime(2023,12,23)
     date_diff = end_date - start_date
     numdays = date_diff.days 
@@ -141,8 +145,8 @@ if __name__ == "__main__":
             date_str = temp_date.strftime("%Y-%m-%d")
             date_list.append(date_str)
 
-    # run_process("2018-01-02")
+    # run_process("2021-02-12")
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=cpu) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=16) as executor:
         # Submit the processing tasks to the ThreadPoolExecutor
         processed_weeks_futures = [executor.submit(run_process, date_str) for date_str in date_list]
