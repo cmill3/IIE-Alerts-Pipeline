afd154d203d3dce764d8b94190bdde96b57a7cb8
cm3 <cfmiller1397@gmail.com>
2024-01-04 22:44:28 -0500

    lcl

--- historical_dataset_builder.py
+++ historical_dataset_builder.py
@@ -129,7 +129,7 @@ def pull_df(date_stamp, prefix, hour):
 if __name__ == "__main__":
     # build_historic_data(None, None)
     cpu = os.cpu_count()
-    start_date = datetime(2021,9,1)
+    start_date = datetime(2021,1,1)
     end_date = datetime(2023,12,23)
     date_diff = end_date - start_date
     numdays = date_diff.days 
